2a4dde0e157626a55a64ce21e7f2fc07a5520a67
CHdev <codigo.turismointeractivo360@gmail.com>
2024-08-08 22:55:28 -0500

    Preparar para despliegue en Vercel

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,18 +16,18 @@ export default {
       indigo: colors.indigo,
       neutral: colors.neutral,  // Used mainly for text color
       yellow: {
-        50: "#fefce8",
-        100: "#fef9c3",
-        400: "#facc15",
-        500: "#eab308",
+        50: "#6aa8fa",
+        100: "#5698ed",
+        400: "#3c87e8",
+        500: "#1a73e8",
       }, // Accent colors, used mainly for star color, heading and buttons
       orange: {
-        100: "#ffedd5",
-        200: "#fed7aa",
-        300: "#fb713b",
-        400: "#fa5a15",
-        500: "#e14d0b",
-        600: "#ea580c",
+        100: "#1a73e8",
+        200: "#1a73e8",
+        300: "#1a73e8",
+        400: "#1a73e8",
+        500: "#1a73e8",
+        600: "#1a73e8",
       }, // Primary colors, used mainly for links, buttons and svg icons
       red: colors.red, // Used for bookmark icon
       zinc: colors.zinc, // Used mainly for box-shadow
